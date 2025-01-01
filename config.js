21b9e57ab6ee2e26aceb4b09e404e669636b107d
funny458 <funnykitten458@gmail.com>
2025-01-01 20:41:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "yrJC3D4C#AppqKGu6Blij1F-AYTHiqQF2vdEeCJi6V63GV26dt-c",
+SESSION_ID: process.env.SESSION_ID || "zrpV0RxB#kFBFLX_om95xPuaNTo3gfX1sSL66NCSc2n6StzeLZUY",
 MONGODB: process.env.MONGODB || "mongodb://mongo:zVaTZTYXwMZzdGDKYLJzjtBPHJASnnyW@junction.proxy.rlwy.net:38558"
 };
